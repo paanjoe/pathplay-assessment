3362e09620b9b4406f1124e5408f756f9a65a30c
PaanJoe <farhan.gunz@gmail.com>
2022-10-28 12:15:04 +0800

    push :

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,47 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
-import React, { useRef } from "react";
-import { usePreserveScroll } from "./scrollctx";
+import React, { useEffect, useRef } from "react";
+import { useRouter } from "next/router";
 
+
+const usePreserveScroll = () => {
+  const router = useRouter()
+
+  const scrollPositions = useRef<{ [url: string]: number }>({})
+  const isBack = useRef(false)
+
+  useEffect(() => {
+    router.beforePopState(() => {
+      isBack.current = true
+      return true
+    })
+
+    const onRouteChangeStart = () => {
+      const url = router.pathname
+      scrollPositions.current[url] = window.scrollY
+    }
+
+    const onRouteChangeComplete = (url: any) => {
+      if (isBack.current && scrollPositions.current[url]) {
+        window.scroll({
+          top: scrollPositions.current[url],
+          behavior: "auto",
+        })
+      }
+
+      isBack.current = false
+    }
+
+    router.events.on("routeChangeStart", onRouteChangeStart)
+    router.events.on("routeChangeComplete", onRouteChangeComplete)
+
+    return () => {
+      router.events.off("routeChangeStart", onRouteChangeStart)
+      router.events.off("routeChangeComplete", onRouteChangeComplete)
+    }
+  }, [router])
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   usePreserveScroll();
